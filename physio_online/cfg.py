36d86fe95d9645d8ffeef1f8388cc61cc41810fb
Brett Graham <graham@rowland.harvard.edu>
2011-07-07 14:15:57 -0400

    adding configuration file system and core aka headless module

---
+++ physio_online/cfg.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import ConfigParser, io, logging, os
+
+# blank items will be filled in later (in set_session)
+# use %% for a % character
+CFGDEFAULTS = """
+[mworks]
+conduitname: server_event_conduit
+conv: 0.000001
+
+[pixel clock]
+socketTemplate: ipc:///tmp/pixel_clock/%%i
+socketStart: 0
+socketStop: 4
+maxError: 2
+
+[audio]
+socketTemplate: tcp://127.0.0.1:%%i
+socketStart: 8000
+socketEnd: 8032
+sampRate: 44100
+"""
+
+class Config(ConfigParser.SafeConfigParser):
+    def __init__(self, *args, **kwargs):
+        ConfigParser.SafeConfigParser.__init__(self, *args, **kwargs)
+        # read in defaults
+        self.readfp(io.BytesIO(CFGDEFAULTS))
+    
+    def read_user_config(self, homeDir=os.getenv('HOME')):
+        filename = '/'.join((homeDir,'.physio_online'))
+        if os.path.exists(filename):
+            logging.debug("Found user cfg: %s" % filename)
+            self.read(filename)
+        else:
+            logging.warning('No user cfg found: %s' % filename)
+    
+    # def read_session_config(self, session):
+    #     filename = '/'.join((self.get('filesystem','datarepo'),session,'physio.ini'))
+    #     if os.path.exists(filename):
+    #         logging.debug("Found session cfg: %s" % filename)
+    #         self.read(filename)
+    #     else:
+    #         logging.warning('No session cfg found: %s' % filename)
+    # 
+    # def set_session(self, session):
+    #     self.set('session','name',session)
+    #     
+    #     if self.get('session','dir').strip() == '':
+    #         self.set('session','dir','/'.join((self.get('filesystem','datarepo'),session)))
+    #     
+    #     if self.get('session','output').strip() == '':
+    #         self.set('session','output','/'.join((self.get('session','dir'),'processed')))
+    #     
+    #     if self.get('mworks','file').strip() == '':
+    #         self.set('mworks','file','/'.join((self.get('session','dir'),session + self.get('mworks','ext'))))
+    #     
+    #     if self.get('pixel clock','output').strip() == '':
+    #         self.set('pixel clock','output','/'.join((self.get('session','output'),'pixel_clock')))
---
+++ physio_online/core.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+"""
+This is the core physio_online object
+"""
+
+import zmq
+
+# import mworks conduit
+sys.path.append("/Library/Application Support/MWorks/Scripting/Python")
+from mworks.conduit import IPCClientConduit
+
+from spike_listener import SpikeListener
+from clock_sync import ClockSync
+from stimsorter import Stim, StimSpikeSyncer
+
+class Core(object):
+    def __init__(self, config, zmqContext=None):
+        if zmqContext is None:
+            zmqContext = zmq.Context()
+        
+        # make clock synchronizer
+        pathFunc = lambda i: config.get('pixel clock', 'socketTemplate') % i
+        channels = range(config.getint('pixel clock', 'socketStart'), config.getint('pixel clock', 'socketEnd'))
+        self.clockSync = ClockSync(pathFunc, channels, zmqContext=zmqContext, maxErr=config.getint('pixel clock', 'maxError'))
+        
+        # make stim spike syncer
+        self.stimSpikeSyncer = StimSpikeSyncer()
+        
+        # make mworks conduit
+        self.mw_conduit = IPCClientConduit(config.get('mworks','conduitname'))
+        self.mw_conduit.initialize()
+        self.mw_conduit.register_local_event_code(0,'#stimDisplayUpdate')
+        self.mw_conduit.register_callback_for_name('#stimDisplayUpdate', self.process_mw_event)
+        
+        # make spike listener
+        # pathFunc = lambda i : "tcp://127.0.0.1:%i" % (i+8000) 
+        pathFunc = lambda i : config.get('audio', 'socketTemplate') % i
+        channels = range(config.getint('audio', 'socketStart'), config.getint('audio', 'socketEnd'))
+        spikeListener = SpikeListener(pathFunc, channels, zmqContext=zmqContext)
+        spikeListener.register_callback(self.process_spike)
+        
+    
+    def process_mw_event(self, event):
+        # global stimSpikeSyncer, clockSync
+        if event is None:
+            return
+        else:
+            event.value = event.data
+        self.stimSpikeSyncer.process_mw_event(event)
+        self.clockSync.process_mw_event(event)
+    
+    def process_spike(self, wb):
+        # global stimSpikeSyncer, clockSync
+
+        if not (self.clockSync.offset is None):
+            # spikeMWTime = clockSync.clockSync.au_to_mw(wb.time_stamp/44100.)
+            self.stimSpikeSyncer.process_spike(wb.channel_id, self.clockSync.au_to_mw(wb.time_stamp/44100.))
+        else:
+            logging.warning("Clock not synced!! dropping spike on %i" % wb.channel_id)
+    
+    def update(self):
+        """
+        Updates the various components of the physio_online core, should be called in the main loop
+        """
+        # global clockSync, sl, stimSpikeSyncer
+        while self.spikeListener.update():
+            pass
+        while self.clockSync.update():
+            pass
+        self.clockSync.match()
+        if self.clockSync.offset is None:
+            logging.debug("MW: %s" % str([e[1] for e in clockSync.mwEvents]))
+            logging.debug("AU: %s" str([e[1] for e in clockSync.auEvents]))
+    
+    def clear_spikes(self):
+        """
+        Clears all spikes accumulated so far
+        """
+        self.stimSpikeSyncer.clear_spikes()
+    
+    def clear_stimuli(self):
+        self.stimSpikeSyncer.clear_stimuli()
+        
